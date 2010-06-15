d8cac1b83a22d2e7b37fc87e1c8c4ece86ef98b8
Robin Spainhour <robin@robinspainhour.com>
2010-06-15 21:56:56 +0200

    Added rspec-mode submodule

--- robin.el
+++ robin.el
@@ -5,3 +5,7 @@
 (add-to-list 'load-path "~/.emacs.d/elisp/feature-mode")
 (require 'feature-mode)
 (add-to-list 'auto-mode-alist '("\.feature$" . feature-mode))
+
+;; rspec-mode
+(add-to-list 'load-path "~/.emacs.d/elisp/rspec-mode")
+(require 'rspec-mode)
